9f60cd4d3730d93ee6faef5ed9138f52dfc2504f
Henrique Almeida (via Travis CI) <henrique_af1@msn.com>
2020-06-22 15:29:35 +0000

    Deploy henriqueaf/clone-discord to github.com/henriqueaf/clone-discord.git:gh-pages

---
+++ precache-manifest.7c7d61419c002866908adf179e59ff42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5401f19f64a84478da4c5c246275f4",
+    "url": "/clone-discord/index.html"
+  },
+  {
+    "revision": "e3191776577f3678bdb9",
+    "url": "/clone-discord/static/js/2.2bd6f369.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone-discord/static/js/2.2bd6f369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04612b5f0a40c2e1fd9",
+    "url": "/clone-discord/static/js/main.da614342.chunk.js"
+  },
+  {
+    "revision": "5c27f8d10f75d0711f0a",
+    "url": "/clone-discord/static/js/runtime-main.6f54f3ca.js"
+  },
+  {
+    "revision": "3fbf5a84d4f02dda562cd0a2775e6727",
+    "url": "/clone-discord/static/media/logo-rocketseat.3fbf5a84.svg"
+  }
+]);
